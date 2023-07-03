7e819baa14550b0e53005a5dfd9ca4367a3eb667
Marina Vasileva <mv031a@yandex.ru>
2023-07-03 09:02:41 +0400

    feat:  Добавить функциональность

---
+++ app.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+
+
+const { celebrate, Joi, errors  } = require('celebrate');
+const express = require('express');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+
+const router = require('./routes/index');
+const { errorServer }  = require('./middlewares/errorServer');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const corsconf = require('./middlewares/corsconf');
+const limiter = require('./middlewares/ratelimit');
+
+
+const { PORT = 3000,
+  MONGODB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb',
+  } = process.env;
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    console.log('Успешное соединение с базой данных');
+  })
+  .catch((err) => {
+    console.log(`Ошибка соединения с базой данных ${err}`);
+  });
+
+const app = express();
+app.use(helmet());
+
+
+app.use(corsconf);
+app.use(cookieParser());
+app.use(requestLogger);
+app.use(limiter);
+app.use(helmet());
+app.use('/', router);
+app.use(errorLogger);
+app.use(errors());
+app.use(errorServer);
+app.listen(PORT, () => {
+  console.log(`Слушаем порт ${PORT}`);
+});
